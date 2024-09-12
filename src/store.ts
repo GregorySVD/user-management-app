d04a64a1e31f271089a942a8d3a85d9889e892e0
GregorySVD <grzegorzterenda@gmail.com>
2024-09-12 18:42:05 +0200

    Implemented modal for cookies acceptation

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './features/users/usersSlice';
 import themeReducer from './features/theme/themeSlice';
+import filterReducer from './features/filter/filterSlice';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     theme: themeReducer,
+    filters: filterReducer,
   },
 });
 
