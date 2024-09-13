2eca174af9ab642b56d86304c974c1fbbae203ee
GregorySVD <grzegorzterenda@gmail.com>
2024-09-13 20:20:32 +0200

    Fix double fetching of users: removed redundant fetch call from UsersTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { UsersTable } from './components/UsersTable/UsersTable';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './store';
-import { Navbar } from './components/Navbar/Navbar';
-import { Footer } from './components/Footer';
 import { useEffect } from 'react';
-import { CookieConsent } from './components/CookieConsent';
-import { AboutSection } from './components/AboutSection/AboutSection';
-import { SearchSection } from './components/SearchSection/SearchSection';
+import { fetchUsers } from './features/users/usersSlice';
 import { setEmailFilter, setNameFilter, setPhoneFilter, setUsernameFilter } from './features/filter/filterSlice';
+import { LoadingLayout } from './components/LoadingLayout';
+import { ErrorLayout } from './components/ErrorLayout';
+import { CookieConsent } from './components/CookieConsent';
+import { Navbar } from './components/Navbar/Navbar';
+import { AboutSection } from './components/AboutSection/AboutSection';
 import { StackUsedSection } from './components/StackUsedSection';
+import { SearchSection } from './components/SearchSection/SearchSection';
+import { UsersTable } from './components/UsersTable/UsersTable';
+import { Footer } from './components/Footer';
 
-function App() {
+export const App = () => {
   const theme = useSelector((state: RootState) => state.theme.theme);
   const dispatch = useDispatch<AppDispatch>();
+
+  // Filters
   const { name, username, email, phone } = useSelector((state: RootState) => state.filters);
+
+  // Redux state for users
+  const { loading, error, users } = useSelector((state: RootState) => state.users);
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -22,6 +30,9 @@ function App() {
       document.body.classList.remove('dark');
     }
   }, [theme]);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -44,14 +55,23 @@ function App() {
     }
   };
 
+  // Handling loading state
+  if (loading) {
+    return <LoadingLayout />;
+  }
+  // Handling error state
+  if (error) {
+    return <ErrorLayout error={new Error(error)} reset={() => dispatch(fetchUsers())} />;
+  }
+
+  // Check if any filter is active
   const isAnyFilterActive = !!(name || username || email || phone);
 
+  // Render the main UI
   return (
     <section>
       <div
-        className={`${
-          theme === 'dark' ? `dark` : ''
-        } bg-bg-100 dark:bg-bg-dark-100 dark:text-text-dark-100 min-w-[300px] flex flex-col min-h-screen`}
+        className={`bg-gradient-light dark:bg-gradient-dark dark:bg-bg-dark-100 dark:text-text-dark-100 text-text-100 min-w-[300px] flex flex-col min-h-screen`}
       >
         <CookieConsent />
         <Navbar />
@@ -70,6 +90,4 @@ function App() {
       </div>
     </section>
   );
-}
-
-export default App;
+};
