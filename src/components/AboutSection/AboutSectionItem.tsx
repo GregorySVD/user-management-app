6c0fd77ac701f852ede7a60ad1555f6680d83a18
GregorySVD <grzegorzterenda@gmail.com>
2024-10-17 19:25:18 +0200

    fix: add hover animation trigger on label for AboutSectionItem component
    - Wrapped the main content of AboutSectionItem in a label
    - Applied group and group-hover classes to trigger the wiggle animation on hover

--- src/components/AboutSection/AboutSectionItem.tsx
+++ src/components/AboutSection/AboutSectionItem.tsx
@@ -10,12 +10,14 @@ export const AboutSectionItem = (props: AboutSectionItemProps) => {
   const { icon: Icon, heading, description } = props;
 
   return (
-    <div className="flex flex-col items-center text-center  bg-bg-200 dark:bg-bg-dark-200 rounded-lg shadow-md p-6 hover:animate-wiggle-more">
-      <div className="bg-primary/10 p-3 rounded-full mb-4">
-        <Icon className="h-6 w-6 text-accent-100 dark:text-accent-dark-100 font-bold" />
+    <label className="flex flex-col items-center text-center group">
+      <div className="bg-bg-200 dark:bg-bg-dark-200 rounded-lg shadow-md p-6 group-hover:animate-wiggle-more">
+        <div className="bg-primary/10 p-3 rounded-full mb-4 ">
+          <Icon className="h-8 w-8 text-accent-100 dark:text-accent-dark-100 font-bold" />
+        </div>
+        <h3 className="text-xl font-semibold mb-2">{heading}</h3>
+        <p className="text-text-200 dark:text-text-dark-200">{description}</p>
       </div>
-      <h3 className="text-xl font-semibold mb-2">{heading}</h3>
-      <p className=" text-text-200 dark:text-text-dark-200">{description}</p>
-    </div>
+    </label>
   );
 };
