da7cff1cdb4e935c1447d6084302bb53e7c78a2f
GregorySVD <grzegorzterenda@gmail.com>
2024-10-17 19:29:49 +0200

    update: make font in navbar links bigger

--- src/components/Navbar/DesktopNavMenu.tsx
+++ src/components/Navbar/DesktopNavMenu.tsx
@@ -8,7 +8,7 @@ interface DesktopNavMenuProps {
 export const DesktopNavMenu: React.FC<DesktopNavMenuProps> = ({ activeSection }) => {
   return (
     <div className="navbar-center hidden lg:flex">
-      <ul className="menu menu-horizontal px-1">
+      <ul className="menu menu-horizontal px-1 text-xl">
         <NavLinks activeSection={activeSection} />
       </ul>
     </div>
